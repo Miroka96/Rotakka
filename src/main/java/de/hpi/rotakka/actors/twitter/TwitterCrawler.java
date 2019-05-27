a70b451f0793f77d513e4cdf9a5e0ce9de8d7c4d
Marvin Thiele <MarvinThiele@gmx.net>
2019-05-27 20:28:29 +0200

    Merge

--- src/main/java/de/hpi/rotakka/actors/twitter/TwitterCrawler.java
+++ src/main/java/de/hpi/rotakka/actors/twitter/TwitterCrawler.java
@@ -1,5 +1,9 @@
 package de.hpi.rotakka.actors.twitter;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -13,7 +17,18 @@ public class TwitterCrawler {
         webDriver = createSeleniumWebDriver(false, chromeOptions);
     }
 
-    public void testMe(String url) {
+    public void extract(String url) {
         webDriver.get(url);
+
+        //List<WebElement> tweets = webDriver.findElement(By.id("stream-items-id")).findElements(By.tagName("li"));
+        Document twPage = Jsoup.parse(webDriver.getPageSource());
+        Elements tweets = twPage.select("ol[id=stream-items-id] li[data-item-type=tweet]");
+
+        for(Element tweet : tweets) {
+            int i = 2;
+            //tweet.childNodes.get(1).attributes()
+            //tweet.select("div.tweet.js-stream-tweet.js-actionable-tweet.js-profile-popup-actionable.dismissible-content.original-tweet.js-original-tweet.tweet-has-context.has-cards.cards-forward")
+        }
+        int debug = 1;
     }
 }

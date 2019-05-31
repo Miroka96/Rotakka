b6f92c2ba6e88819b397e130c53c9e2c08eb2a45
Marvin Thiele <MarvinThiele@gmx.net>
2019-05-31 11:02:02 +0200

    Crunchbase Company Date Retriever

--- src/main/java/de/hpi/rotakka/MainTest.java
+++ src/main/java/de/hpi/rotakka/MainTest.java
@@ -1,42 +1,39 @@
 package de.hpi.rotakka;
 
-import de.hpi.rotakka.actors.proxy.ProxyWrapper;
-import de.hpi.rotakka.actors.twitter.TwitterCrawler;
-
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.URL;
-import java.net.URLConnection;
+import de.hpi.rotakka.actors.proxy.crawling.websites.CrawlerFreeProxyLists;
 
 // This class can be used to test single components
 public class MainTest {
 
     public static void main(String[] args) {
 
-        // Test the new availablility check
-        try {
-            ProxyWrapper proxy = new ProxyWrapper("190.114.254.171", 8080, "HTTP");
-            InetAddress address = InetAddress.getByName(proxy.getIp());
-            boolean reachable = address.isReachable(10000);
-            System.out.println(reachable);
-            if (reachable) {
-                URLConnection connection = new URL("http://www.google.com").openConnection(proxy.getProxyObject());
-                connection.setConnectTimeout(10000);
-                connection.connect();
-                Object content = connection.getContent();
-                System.out.println("works!");
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        CrawlerFreeProxyLists a = new CrawlerFreeProxyLists();
+        a.extract();
 
-        TwitterCrawler twC = new TwitterCrawler();
-        twC.extract("https://twitter.com/elonmusk");
-        // Test the RotakkarProxy Crawler
-        //CrawlerFreeProxyLists crawler = new CrawlerFreeProxyLists();
-        //&List<RotakkaProxy> proxies = crawler.extract();
-        //System.out.println("Found proxies: "+proxies.size());
+//        // Test the new availablility check
+//        try {
+//            ProxyWrapper proxy = new ProxyWrapper("190.114.254.171", 8080, "HTTP");
+//            InetAddress address = InetAddress.getByName(proxy.getIp());
+//            boolean reachable = address.isReachable(10000);
+//            System.out.println(reachable);
+//            if (reachable) {
+//                URLConnection connection = new URL("http://www.google.com").openConnection(proxy.getProxyObject());
+//                connection.setConnectTimeout(10000);
+//                connection.connect();
+//                Object content = connection.getContent();
+//                System.out.println("works!");
+//            }
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        TwitterCrawler twC = new TwitterCrawler();
+//        twC.extract("https://twitter.com/elonmusk");
+//        // Test the RotakkarProxy Crawler
+//        //CrawlerFreeProxyLists crawler = new CrawlerFreeProxyLists();
+//        //&List<RotakkaProxy> proxies = crawler.extract();
+//        //System.out.println("Found proxies: "+proxies.size());
 
 
     }

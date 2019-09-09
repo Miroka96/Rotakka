0813b39936a5db0bb296586d6606fa3c8be1edb2
mirko.krause <mirko.krause@student.hpi.de>
2019-09-09 15:32:27 +0200

    added file logging on debug level and console logging on info level

--- src/main/java/de/hpi/rotakka/actors/data/graph/util/ShardMapper.java
+++ src/main/java/de/hpi/rotakka/actors/data/graph/util/ShardMapper.java
@@ -87,7 +87,6 @@ public class ShardMapper {
     public void assign(@NotNull ActorRef slave, int shard, boolean ready) {
         String slaveName = slave.toString();
         int slashIndex = slaveName.lastIndexOf('/');
-        //int hashIndex = slaveName.lastIndexOf('#');
         slaveName = slaveName.substring(slashIndex + 1, slaveName.length() - 1).replace('#', '_');
         String bufferName = GraphStoreBuffer.DEFAULT_NAME + "_" + shard + "_" + slaveName;
         ActorRef buffer;
